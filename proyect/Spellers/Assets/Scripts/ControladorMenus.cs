89d12fb79807545680357dc653ac057eba3f544d
MarcosMD15 <55489396+MarcosMD15@users.noreply.github.com>
2021-10-07 01:57:13 +0200

    Implementaciones nuevas
    Implementado botón de pantalla completa, esqueleto del menú opciones y el botón de cerrar el juego

--- proyect/Spellers/Assets/Scripts/ControladorMenus.cs
+++ proyect/Spellers/Assets/Scripts/ControladorMenus.cs
@@ -1,27 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControladorMenus : MonoBehaviour
 {
-    //Variables
+    ////////////////////
+    //// Variables ////
+    //////////////////
+    
     public GameObject menuPrincipal;
     public GameObject menuJugar;
     public GameObject menuOpciones;
     public GameObject menuCreditos;
 
+    public GameObject fuentePequeñaObj;
+    public GameObject fuenteMedianaObj;
+    public GameObject fuenteGrandeObj;
+    Toggle fuentePequeña;
+    Toggle fuenteMediana;
+    Toggle fuenteGrande;
+
+    public GameObject pantallaCompleta;
+
+    //////////////////
+    //// Metodos ////
+    ////////////////
+
     // Start is called before the first frame update
     void Start()
     {
         menuPrincipal.SetActive(true);
+        fuentePequeña = fuentePequeñaObj.GetComponent<Toggle>();
+        fuenteMediana = fuenteMedianaObj.GetComponent<Toggle>();
+        fuenteGrande = fuenteGrandeObj.GetComponent<Toggle>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Screen.fullScreen)
+        {
+            pantallaCompleta.SetActive(true);
+        }
+        else
+        {
+            pantallaCompleta.SetActive(false);
+        }
     }
 
+    //// Metodo para todos los Menus ////
+    public void clickPantallaCompleta()
+    {
+        Screen.fullScreen = false;
+    }
+
+    //// Metodos para el Menu Principal ////
     public void clickJugar()
     {
         menuJugar.SetActive(true);
@@ -40,6 +74,12 @@ public class ControladorMenus : MonoBehaviour
         menuPrincipal.SetActive(false);
     }
 
+    public void clickSalir()
+    {
+        Debug.Log("Salir");
+    }
+
+    //// Metodo para los Menus que no sen el Principal ////
     public void clickAtras()
     {
         menuPrincipal.SetActive(true);
